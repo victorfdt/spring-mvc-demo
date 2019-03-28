34bc4d309a538c123ee0bf4269c9ca2b265bad6b
Victor Di Trani <victorfdt@yahoo.com.br>
2019-03-28 09:29:40 +0000

    Add @InitBinder annotation to remove the whitespaces from all incoming Strings present in the request

--- src/com/victorfdt/mvc/controller/CustomerController.java
+++ src/com/victorfdt/mvc/controller/CustomerController.java
@@ -2,9 +2,12 @@ package com.victorfdt.mvc.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -31,5 +34,21 @@ public class CustomerController {
 
 			return "customer-confirmation";
 		}
+	}
+
+	/**
+	 * This method is called on every incoming request.
+	 * It has a custom editor for removing white spaces.
+	 * @param dataBinder
+	 */
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+
+		// Class available at Spring API
+		// the true value in the constructor, turn empty words to null
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+		// for every String class, apply the stringTrimmerEditor
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
 	}
 }
